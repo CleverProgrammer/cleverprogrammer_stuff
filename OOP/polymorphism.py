334e7608d35171082bccdd43095119b0aa0d9961
Rafeh Qazi <rafehqazi1@gmail.com>
2017-01-01 19:46:48 -0600

    Organize the code better.

--- OOP/polymorphism.py
+++ OOP/polymorphism.py
@@ -5,23 +5,24 @@ class Animal:
     def talk(self):
         pass
 
-pet_qazi = Animal("Qazoo")
-print(pet_qazi.name)
-print(pet_qazi.talk())
 
 class Dog(Animal):
     def talk(self):
         return "WOOF"
 
 
-pet = Dog("Dot")
-print(pet.name)
-print(pet.talk())
-
 class Cat(Animal):
     def talk(self):
         return "Meow"
 
+
+pet_qazi = Animal("Qazoo")
+print(pet_qazi.name)
+print(pet_qazi.talk())
+
+pet = Dog("Dot")
+print(pet.name)
+print(pet.talk())
 
 kitten = Cat("Brian")
 print(kitten.name)
