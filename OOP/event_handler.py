2ef3a6899eff813dbf9fe72ea37a90a9341a38b8
Rafeh Qazi <rafehqazi1@gmail.com>
2017-01-02 04:19:56 -0600

    Event handler lesson.

---
+++ OOP/event_handler.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+
+def doorbell(event):
+    print("You rang the doorbell!!")
+
+
+window = tk.Tk()
+window.geometry("300x200")
+
+alabel = tk.Label(text="Banana")
+alabel.grid(column=0, row=0)
+
+blabel = tk.Label(text="Apple")
+blabel.grid(column=1, row=0)
+
+button = tk.Button(window, text="Doorbell")
+button.grid(column=0)
+
+button2 = tk.Button(window, text="10")
+button2.grid(column=1, row=1)
+
+button.bind("<Button-1>", doorbell)
+
+window.mainloop()
