edf0aa5864d5cf2f0a96bb52339f702551f93877
Rafeh Qazi <rafehqazi1@gmail.com>
2017-01-25 01:22:49 -0600

    Complete age calculator app with images, entries, grid layout, text, and button.

---
+++ OOP/age_calculator_app.py
@@ -0,0 +1,89 @@
+from PIL import Image, ImageTk
+import datetime  # we will use this for date objects
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Age Calculator APP")
+
+window.geometry('400x400')
+
+
+class Person:
+    def __init__(self, name, surname, birthdate, address, telephone, email):
+        self.name = name
+        self.surname = surname
+        self.birthdate = birthdate
+
+        self.address = address
+        self.telephone = telephone
+        self.email = email
+
+    def age(self):
+        today = datetime.date.today()
+        age = today.year - self.birthdate.year
+
+        if today < datetime.date(today.year, self.birthdate.month, self.birthdate.day):
+            age -= 1
+
+        return age
+
+
+person = Person(
+    "Jane",
+    "Doe",
+    datetime.date(1992, 3, 12),  # year, month, day
+    "No. 12 Short Street, Greenville",
+    "555 456 0987",
+    "jane.doe@example.com"
+)
+
+print(person.name)
+print(person.email)
+print(person.age())
+
+year_label = tk.Label(text="Year")
+year_label.grid(column=0, row=1)
+
+month_label = tk.Label(text="Month")
+month_label.grid(column=0, row=2)
+
+day_label = tk.Label(text="Day")
+day_label.grid(column=0, row=3)
+
+birth_year = tk.Entry(master=window)
+birth_year.grid(column=1, row=1)
+
+birth_month = tk.Entry(master=window)
+birth_month.grid(column=1, row=2)
+
+birth_day = tk.Entry(master=window)
+birth_day.grid(column=1, row=3)
+
+
+def calculate_age():
+    print(birth_year.get())
+    print(birth_month.get())
+    print(birth_day.get())
+    qazi = Person('bob', 'qazi', datetime.date(int(birth_year.get()),
+                                               int(birth_month.get()),
+                                               int(birth_day.get())),
+                  'blah', '323', '@gmail.com')
+    print(qazi.age())
+    text_answer = tk.Text(master=window, height=20, width=30)
+    text_answer.grid(column=1, row=5)
+    answer_text = "You are {age} years old!".format(age=qazi.age())
+    text_answer.insert(tk.END, answer_text)
+    text_answer.tag_configure('bold_italics', font=('Helvetica', 12, 'bold', 'italic'))
+
+
+button1 = tk.Button(text="Calculate Now!", command=calculate_age)
+button1.grid(column=1, row=4)
+
+image = Image.open('/Users/ChessTastic/Documents/Clever Programmer/Brand/Pictures/cp_website_coding.jpg')
+image.thumbnail((100, 100), Image.ANTIALIAS)
+photo = ImageTk.PhotoImage(image)
+label_image = tk.Label(image=photo)
+label_image.grid(column=1, row=0)
+# print(get_entry())
+
+window.mainloop()
