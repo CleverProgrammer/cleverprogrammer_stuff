0dfa843c299717a6d6a222e2e244c4b7e894aa4b
Rafeh Qazi <rafehqazi1@gmail.com>
2017-01-31 23:52:40 -0600

    Add pack geometry manager lesson and root example.

---
+++ OOP/deep_dive_tkinter/pack_geometry_manager.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+label1 = tk.Label(root, text='BANANA')
+button1 = tk.Button(root, text='Button!')
+label1.pack()
+button1.pack()
+
+root.mainloop()
---
+++ OOP/deep_dive_tkinter/root_example.py
@@ -0,0 +1,6 @@
+# create root window
+# TCL -- Tools Command Line
+import tkinter as tk
+
+root = tk.Tk()
+root.mainloop()
